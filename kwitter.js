0cb508ae413b0965170e9e0c9d34372b4eac57a0
BOZCOBEL10 <116418092+BOZCOBEL10@users.noreply.github.com>
2023-06-15 11:22:27 +0400

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,16 @@
+function addUser(){
+        user_name = document.getElementById("user_name").value;
+
+        localStorage.setItem("user_name",user_name);
+    
+        window.location = "kwitter_room.html";
+
+        firebase.database().ref("/").child(user_name).update({
+            purpose:"adding user"
+        });
+}
+
+function cirdp(){
+    window.location="bird-dance.html";
+}
+
